46012e7d321618debdc8062884f49886d7c330aa
Vantol Bennett (titan) <vvantol2000@icloud.com>
2020-11-17 23:09:16 -0500

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
 
       <main className={styles.main}>
